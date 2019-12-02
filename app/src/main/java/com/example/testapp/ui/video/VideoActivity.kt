ff10f7ac8e3156b8c123ab8e2c12461556745315
Admin <syed.sakib143@gmail.com>
2019-12-02 23:16:21 +0530

    All files commited by Sakib

--- app/src/main/java/com/example/testapp/ui/video/VideoActivity.kt
+++ app/src/main/java/com/example/testapp/ui/video/VideoActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvm_demo.utils.toast
 import com.example.testapp.R
 
 import com.google.android.youtube.player.YouTubeBaseActivity
@@ -15,36 +16,49 @@ import kotlinx.android.synthetic.main.activity_video.*
 class VideoActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
 
-    override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer?, wasRestored: Boolean) {
-        showShortToast("Youtube Api Initialization Success")
+    override fun onInitializationSuccess(
+        provider: YouTubePlayer.Provider?,
+        player: YouTubePlayer?,
+        wasRestored: Boolean
+    ) {
+        toast("Youtube Api Initialization Success")
         if (!wasRestored) {
             player?.cueVideo("57YjbMStSUk");
         }
     }
 
-    override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
-        showShortToast("Youtube Api Initialization Failure")
+    override fun onInitializationFailure(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubeInitializationResult?
+    ) {
+        toast("Youtube Api Initialization Failure")
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
-        yt_video1.initialize("AIzaSyBrFISewnoB1y9qtOvBOuXSGfdjruOmUJM", this)
-        /*yt_video2.initialize("AIzaSyBrFISewnoB1y9qtOvBOuXSGfdjruOmUJM", this)
-        yt_video3.initialize("AIzaSyBrFISewnoB1y9qtOvBOuXSGfdjruOmUJM", this)*/
 
-        btnYoutubeChannel.setOnClickListener {
+        setYoutubeData()
+        setClickListner()
+    }
+
+    private fun setClickListner() {
+        btnMoreVideosVA.setOnClickListener {
             val urlYoutube = "https://www.youtube.com/channel/UCjevcnwURum6weHrnkHul4Q"
             val urlYoutubeInApp = "vnd.youtube.com/channel/UCjevcnwURum6weHrnkHul4Q"
             try {
-                //here we try to open the link in app
                 startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(urlYoutubeInApp)))
             } catch (e: Exception) {
-                //the app isn't available: we open in browser`
                 startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(urlYoutube)))
             }
         }
+    }
+
+    private fun setYoutubeData() {
+        videoOneVA.initialize("AIzaSyBrFISewnoB1y9qtOvBOuXSGfdjruOmUJM", this)
+        videoTwoVA.initialize("AIzaSyBrFISewnoB1y9qtOvBOuXSGfdjruOmUJM", this)
+        videoThreeVA.initialize("AIzaSyBrFISewnoB1y9qtOvBOuXSGfdjruOmUJM", this)
     }
 
 }
