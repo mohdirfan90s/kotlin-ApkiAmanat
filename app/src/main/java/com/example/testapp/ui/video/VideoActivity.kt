ff25a2d2bdbfbd0d69480f1dc4c765cc2a00ef19
Irfan Shaikh <mohdirfan90s@gmail.com>
2019-12-02 16:35:58 +0530

    youtube integration

--- app/src/main/java/com/example/testapp/ui/video/VideoActivity.kt
+++ app/src/main/java/com/example/testapp/ui/video/VideoActivity.kt
@@ -8,14 +8,16 @@ import com.example.testapp.R
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView;
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_video.*
 
 class VideoActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
     override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer?, wasRestored: Boolean) {
         showShortToast("Youtube Api Initialization Success")
         if (!wasRestored) {
-            player?.cueVideo("wKJ9KzGQq0w");
+            player?.cueVideo("57YjbMStSUk");
         }
     }
 
@@ -27,7 +29,9 @@ class VideoActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
-        yt_pv.initialize("AIzaSyBrFISewnoB1y9qtOvBOuXSGfdjruOmUJM", this)
+        yt_video1.initialize("AIzaSyBrFISewnoB1y9qtOvBOuXSGfdjruOmUJM", this)
+        /*yt_video2.initialize("AIzaSyBrFISewnoB1y9qtOvBOuXSGfdjruOmUJM", this)
+        yt_video3.initialize("AIzaSyBrFISewnoB1y9qtOvBOuXSGfdjruOmUJM", this)*/
     }
 
 
